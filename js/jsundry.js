fde79ba9de1b50a5e92a38c41ddb0e4c46d19039
Rob Anderson <roboprog@yahoo.com>
2011-08-30 21:06:10 -0700

    use google api to load jquery and run local js code when all is ready

---
+++ js/jsundry.js
@@ -0,0 +1,11 @@
+// library to merge sundry files into completed page
+jsundry =
+{
+// merge template and data
+merge: function()
+	{
+	alert( 'do something in jsundry.merge');
+	}
+}
+// vi: ts=4 sw=4
+// *** EOF ***
