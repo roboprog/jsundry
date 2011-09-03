9fc7ba4b952333b154fd322d0c3445d254e2876b
Rob Anderson <roboprog@yahoo.com>
2011-09-02 19:37:38 -0700

    look for reference to an HTML template file

--- js/jsundry.js
+++ js/jsundry.js
@@ -1,10 +1,35 @@
 // library to merge sundry files into completed page
 jsundry =
 {
-// merge template and data
+// (semi) private functions
+privs:
+{
+// load indicated template
+load: function()
+	{
+	var sub_div;
+	var a_href;
+	var link;
+
+	sub_div = this;
+	a_href = $( 'a', sub_div);
+	if ( a_href.length == 0)
+		{
+		return;
+		}
+
+	link = a_href[ 0 ].getAttribute( 'href');
+	alert( 'Found link \"' + link + '\"');
+	}
+},
+
+// find divs with templates (possibly to be updated with data)
 merge: function()
 	{
-	alert( 'do something in jsundry.merge');
+	var sub_divs;
+
+	sub_divs = $( 'div.subst');
+	sub_divs.each( jsundry.privs.load);
 	}
 }
 // vi: ts=4 sw=4
