8579ab758008740112a8246a1682d613de547e19
Rob Anderson <roboprog@yahoo.com>
2011-09-17 07:46:54 -0700

    stubs for inserting the data content into the template

--- js/jsundry.js
+++ js/jsundry.js
@@ -4,15 +4,51 @@ jsundry =
 // (semi) private functions
 privs:
 {
+// generic ajax error handler
+error: function( x, status, err)
+	{
+	alert( 'Oops: (' + link + ') ' + status + '/' + err);
+	},
+// load indicated content (data)
+load_content: function()
+	{
+	var data_id_div;
+	var a_href;
+	var link;
+	var ajax_args;
+	var continuation;
+
+	data_id_div = this;
+	a_href = $( 'a', data_id_div);
+	if ( a_href.length == 0)
+		{
+		return;
+		}
+
+	link = a_href[ 0 ].getAttribute( 'href');
+	// alert( 'Found link \"' + link + '\"');
+	continuation = function( json)
+		{
+		alert( 'Loaded ' + json);
+		// TODO:  scan for replacements in template
+		};
+	ajax_args =
+		{
+		url: link,
+		content: 'json/text',
+		success: continuation,
+		error: jsundry.privs.error
+		};
+	$.ajax( ajax_args);
+	},
 // load indicated template
-load: function()
+load_template: function()
 	{
 	var sub_div;
 	var a_href;
 	var link;
 	var ajax_args;
-	var success;
-	var error;
+	var continuation;
 
 	sub_div = this;
 	a_href = $( 'a', sub_div);
@@ -23,21 +59,24 @@ load: function()
 
 	link = a_href[ 0 ].getAttribute( 'href');
 	// alert( 'Found link \"' + link + '\"');
-	success = function( html)
+	continuation = function( html)
 		{
+		var sub_divs;
+
 		alert( 'Loaded ' + html);
 		sub_div.innerHTML = $( 'body')[ 0 ].innerHTML;
-		};
-	error = function( x, status, err)
-		{
-		alert( 'Oops: (' + link + ') ' + status + '/' + err);
+
+		// now that template is loaded, insert data into it
+		sub_divs = $( 'div.content');
+		// TODO:  enforce only 1
+		sub_divs.each( jsundry.privs.load_content);
 		};
 	ajax_args =
 		{
 		url: link,
 		content: 'html',
-		success: success,
-		error: error
+		success: continuation,
+		error: jsundry.privs.error
 		};
 	$.ajax( ajax_args);
 	}
@@ -48,8 +87,9 @@ merge: function()
 	{
 	var sub_divs;
 
-	sub_divs = $( 'div.subst');
-	sub_divs.each( jsundry.privs.load);
+	sub_divs = $( 'div.template');
+	// TODO:  enforce only 1
+	sub_divs.each( jsundry.privs.load_template);
 	}
 }
 // vi: ts=4 sw=4
